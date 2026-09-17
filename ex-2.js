1b4d9d842b88e14555a6011cc1e4b3539282fc5f
wittawat2 <wittawatjob2@gmail.com>
2026-09-17 20:54:03 +0700

    fix ex2

--- ex-2.js
+++ ex-2.js
@@ -12,6 +12,6 @@ product['ratings'] = 4.5;
 product['photo'] = "<Link href='https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg'>https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg</Link>"
 product.code = 'IT0101010';
 product['price'] = 6000;
-
+delete product['ratings'];
 product[socialMediaShare] = 45.5;
 console.log(product);
